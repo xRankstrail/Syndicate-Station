404c67a5c1f1d660f174954d8a669e5fe7a5d16f
Lincoln McQueen <lincoln.mcqueen@gmail.com>
2024-08-29 06:13:15 -0500

    Added CQC

--- Content.Shared/_White/Grab/GrabThrownSystem.cs
+++ Content.Shared/_White/Grab/GrabThrownSystem.cs
@@ -99,4 +99,9 @@ public sealed class GrabThrownSystem : EntitySystem
 
         _layingDown.TryLieDown(uid, behavior:DropHeldItemsBehavior.AlwaysDrop);
     }
+
+    public void Throw(EntityUid target, Vector2 dir)
+    {
+        throw new NotImplementedException();
+    }
 }
